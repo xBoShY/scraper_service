4d8b657f8698d6cdbcf6f468efc768939158ae4e
xBoShY <xboshy@gmail.com>
2021-06-06 17:22:29 +0100

    improved test error description

--- main_test.go
+++ main_test.go
@@ -16,7 +16,7 @@ func TestServiceStatusOk(t *testing.T) {
 		MetricsListen: ":0",
 	}
 
-	url := "http://www.phaidra.ai"
+	url := "https://www.phaidra.ai/trackrecord"
 	expectedServiceStatus := http.StatusOK
 	expectedScrapeStatus := http.StatusOK
 
@@ -31,17 +31,66 @@ func TestServiceStatusOk(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 	if resp.StatusCode != expectedServiceStatus {
-		t.Fatalf(`Failed`)
+		t.Fatalf(`Expected Status Code %d, got %d`, expectedServiceStatus, resp.StatusCode)
 	}
 
 	serviceMetrics, err := svc.metrics.GetService(expectedServiceStatus)
-	if err != nil || serviceMetrics != 1 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if serviceMetrics != 1 {
+		t.Fatalf(`Expected service metrics to be 1, got %f`, serviceMetrics)
 	}
 
 	scrapeMetrics, err := svc.metrics.GetScrapes(url, expectedScrapeStatus)
-	if err != nil || scrapeMetrics != 1 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if scrapeMetrics != 1 {
+		t.Fatalf(`Expected scrape metrics to be 1, got %f`, scrapeMetrics)
+	}
+}
+
+func TestServiceStatusRedirect(t *testing.T) {
+	args := Arguments{
+		Listen: ":0",
+		Workers: 1,
+		Timeout: 1000,
+		MetricsListen: ":0",
+	}
+
+	url := "https://google.com"
+	expectedServiceStatus := http.StatusOK
+	expectedScrapeStatus := http.StatusMovedPermanently
+
+	svc := StartService(args)
+	defer svc.metrics.Close()
+	defer svc.scraper.Close()
+
+	json := fmt.Sprintf("{\"url\": \"%s\"}", url)
+	req := strings.NewReader(json)
+	resp, err := http.Post("http://" + svc.scraper.GetListen(), "", req)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if resp.StatusCode != expectedServiceStatus {
+		t.Fatalf(`Expected Status Code %d, got %d`, expectedServiceStatus, resp.StatusCode)
+	}
+
+	serviceMetrics, err := svc.metrics.GetService(expectedServiceStatus)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if serviceMetrics != 1 {
+		t.Fatalf(`Expected service metrics to be 1, got %f`, serviceMetrics)
+	}
+
+	scrapeMetrics, err := svc.metrics.GetScrapes(url, expectedScrapeStatus)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if scrapeMetrics != 1 {
+		t.Fatalf(`Expected scrape metrics to be 1, got %f`, scrapeMetrics)
 	}
 }
 
@@ -68,17 +117,23 @@ func TestServiceStatusTeapot(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 	if resp.StatusCode != expectedServiceStatus {
-		t.Fatalf(`Failed`)
+		t.Fatalf(`Expected Status Code %d, got %d`, expectedServiceStatus, resp.StatusCode)
 	}
 
 	serviceMetrics, err := svc.metrics.GetService(expectedServiceStatus)
-	if err != nil || serviceMetrics != 1 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if serviceMetrics != 1 {
+		t.Fatalf(`Expected service metrics to be 1, got %f`, serviceMetrics)
 	}
 
 	scrapeMetrics, err := svc.metrics.GetScrapes(url, expectedScrapeStatus)
-	if err != nil || scrapeMetrics != 1 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if scrapeMetrics != 1 {
+		t.Fatalf(`Expected scrape metrics to be 1, got %f`, scrapeMetrics)
 	}
 }
 
@@ -108,18 +163,27 @@ func TestWorkerWaitTimeout(t *testing.T) {
 	}
 
 	serviceMetrics, err := svc.metrics.GetService(expectedServiceStatus)
-	if err != nil || serviceMetrics != 1 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if serviceMetrics != 1 {
+		t.Fatalf(`Expected Status Code %d, got %d`, expectedServiceStatus, resp.StatusCode)
 	}
 
 	workerWaitCount, err := svc.metrics.GetCountWorkerWaits()
-	if err != nil || workerWaitCount != 1 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if workerWaitCount != 1 {
+		t.Fatalf(`Expected worker wait metrics to be 1, got %d`, workerWaitCount)
 	}
 
 	workerWaitSum, err := svc.metrics.GetSumWorkerWaits()
-	if err != nil || workerWaitSum < 1000 || workerWaitSum > 1010 {
-		t.Fatalf(`Failed`)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if workerWaitSum < 1000 || workerWaitSum > 1010 {
+		t.Fatalf(`Expected worker wait timeout metrics to be between 1000 and 1010, got %f`, workerWaitSum)
 	}
 }
 
